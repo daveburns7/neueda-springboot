38846258104daed6cec637d115e2b31c1b3aea1f
BUR14091750 <BUR14091750@CLCLRCAC19.belfastmet.int>
2017-11-28 13:28:39 +0000

    Dwarfs and Stuff

---
+++ 03-01-Java-DwarfsAndDwarves/src/Dwarf.java
@@ -0,0 +1,27 @@
+
+public class Dwarf {
+	
+	private String name;
+	private String author;
+	private String image;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+}
---
+++ 03-01-Java-DwarfsAndDwarves/src/Dwarves.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Dwarves {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> dwarfs = new ArrayList<String>();
+
+		dwarfs.add("Doc");
+		dwarfs.add("Grumpy");
+		dwarfs.add("Happy");
+		dwarfs.add("Sleepy");
+		dwarfs.add("Bashful");
+		dwarfs.add("Sneezy");
+		dwarfs.add("Dopey");
+
+		ArrayList<String> dwarves = new ArrayList<String>();
+
+		dwarves.add("Thorin Oakenshield");
+		dwarves.add("Dwalin");
+		dwarves.add("Balin");
+		dwarves.add("Gloin");
+		dwarves.add("Kili");
+		dwarves.add("Fili");
+		dwarves.add("Dori");
+		dwarves.add("Nori");
+		dwarves.add("Ori");
+		dwarves.add("Óin");
+		dwarves.add("Bifur");
+		dwarves.add("Bombur");
+		dwarves.add("Bofur");
+
+		System.out.println("The Disney Dwarfs are: ");
+		for (int i = 0; i < dwarfs.size(); i++) {
+			System.out.println(dwarfs.get(i));
+		}
+
+		System.out.println("The Tolkien Dwarves are: ");
+		for (int i = 0; i < dwarves.size(); i++) {
+			{
+				System.out.println(dwarves.get(i));
+			}
+
+		}
+	}
+}
